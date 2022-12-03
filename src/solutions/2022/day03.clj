fb0e0bc037a8b6a3ee064d75eddd927b301c9591
Ellis Kenyo <me@elken.dev>
2022-12-03 09:26:59 +0000

    fix(day3): generalize

--- src/solutions/2022/day03.clj
+++ src/solutions/2022/day03.clj
@@ -13,21 +13,19 @@
        38
        96)))
 
-(defn check-items [rucksack]
-  (let [[lhs rhs] (partition (/ (count rucksack) 2) rucksack)]
-    (first (set/intersection (set lhs) (set rhs)))))
+(defn split-rucksack [rucksack]
+  (partition (/ (count rucksack) 2) rucksack))
+
+(defn check-items [rucksacks]
+  (first (apply set/intersection (map set rucksacks))))
 
 (defn part-1
   [input]
-  (transduce (map (comp get-priority check-items)) + 0 input))
+  (transduce (map (comp get-priority check-items split-rucksack)) + 0 input))
 
 (defn part-2
   [input]
-  (transduce
-   (map #(get-priority
-          (first
-           (apply set/intersection (map set %)))))
-   + 0 (partition 3 input)))
+  (transduce (map (comp get-priority check-items)) + 0 (partition 3 input)))
 
 (deftest test-answers
   (t/is (= 7845 (part-1 input)))
