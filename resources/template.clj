7e97a2a9c4758af2d83fff1798816fd7a5c21e9c
Ellis Kenyo <me@elken.dev>
2023-08-22 06:53:07 +0100

    fix: adjust space for template
    Looks wrong in the template but since DAY is more chars than the
    resulting day, it looks right in the output

--- resources/template.clj
+++ resources/template.clj
@@ -15,7 +15,7 @@
 ;;
 ;; First things first, let's load our input and parse it
 (def input (->> (slurp (io/resource "inputs/YEAR/dayDAY.txt")) ;; Load the resource
-                str/split-lines))                              ;; Split into lines
+                str/split-lines))                             ;; Split into lines
 {:nextjournal.clerk/visibility {:result :hide}}
 
 ;; ## Part 1
