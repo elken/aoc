2ab74e7305f3b146756ed8efc93f78a7f40d85e0
Ellis Kenyo <me@elken.dev>
2025-12-02 08:35:52 +0000

    feat(1/25): massive simplify

--- src/solutions/2025/day01.clj
+++ src/solutions/2025/day01.clj
@@ -9,7 +9,7 @@
 ;; # Problem
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
 (clerk/html (u/load-problem "01" "2025"))
-{:nextjournal.clerk/visibility {:code :show :result :show}}
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
 
 ;; # Solution
 ;;
@@ -21,53 +21,63 @@
 ;; in. Today's day 1 problem feels a bit much for a day oner, so I'm
 ;; already pre-dreading things...
 ;;
+{:nextjournal.clerk/visibility {:code :hide :result :show}}
+
+(clerk/html [:div.border-l-4.border-yellow-500.bg-yellow-50.dark:bg-yellow-900.rounded.p-4.my-4
+             [:p.text-yellow-800.dark:text-yellow-200 (clerk/md "After some thinking, I have come back to this the day after and realized this _is_ a very simple problem after all...
+
+It just boils down to simple division and counting multiples, sorry Eric!")]])
+
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
+
 ;; Today has us trying to work out a password from some safe
 ;; combination logic. We start the dial at `50` and rotate based on
 ;; the input; so `L68` rotates left by 68 and `R1` rotates right by 1.
 ;;
 ;; Part 1 wants us to record how many times the dial stops at 0; and
 ;; part 2 wants us to record how many times it loops around 0.
-;;
-;; First things first, let's load our input and parse it
-
-(defn replace-chars [input]
-  (str "50"
-       (-> input
-           (str/join)
-           (str/replace "L" " -")
-           (str/replace "R" " "))))
-
-(def input (->> (slurp (io/resource "inputs/2025/day01.txt")) ;; Load the resource
-                str/split-lines))                             ;; Split into lines
-
-{:nextjournal.clerk/visibility {:result :hide}}
 
 ;; ## Functions
 ;;
-;; We define a simple function to handle the rotation parsing for both
-;; parts. I'd love to just be able to do `[head & tail]` destructuring
-;; here but Clojure treats the output as a list always so I'd have to
-;; do the parse-long trick anyway.
-(defn parse-rotation [rot]
-  [(first rot) (parse-long (apply str (rest rot)))])
+;; ### parse-input
+;;
+;; Given a list of all the combinations, convert them to +/- numbers
+;; based on direction.
+(defn parse-input [lines]
+  (map #(read-string (str/replace % #"[LR]" {"L" "-" "R" ""})) lines))
+
+;; ### count-crossings
+;;
+;; Given a `start` and `end`, compute how many multiples of 100 are
+;; between two positions on the dial.
+;;
+;; The problem is a deceptive division operations hiding as a bigger
+;; problem...
+(defn count-crossings [start end]
+  (count (filter #(zero? (mod % 100))
+                 (range start end (compare end start)))))
+
+;; ## Input
+
+;; Now let's load our input and parse it
+
+(def input (->> (slurp (io/resource "inputs/2025/day01.txt")) ;; Load the resource
+                str/split-lines                               ;; Split into lines
+                parse-input                                   ;; Convert to +/- numbers
+                (reductions + 50)))                           ;; Map all the positions of applying `+`
 
 ;; ## Part 1
 ;;
-;; As above, part 1 wants us to just keep track of when we hit 0.
+;; As above, part 1 wants us to just keep track of when we hit 0 or
+;; 100.
 ;;
-;; We compute the new total by using the rotation to move the dial. We
-;; don't have to track the direction yet, so it ends up being very
-;; simple.
+;; So we filter all the values that divide equally into 100 and count
+;; them.
 (defn part-1
   [input]
-  (:zeroes
-   (reduce (fn [{:keys [total zeroes]} rot]
-             (let [[new-dir amount] (parse-rotation rot)
-                   new-total (mod ((case new-dir \L - \R +) total amount) 100)]
-               {:total new-total
-                :zeroes (+ zeroes (if (zero? new-total) 1 0))}))
-           {:total 50 :zeroes 0}
-           input)))
+  (->> input
+       (filter #(zero? (mod % 100)))
+       count))
 
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
@@ -77,19 +87,16 @@
 ;;
 ;; Part 2 also has us tracking when it loops around the bounds (0-100).
 ;;
-;; Similar to part 1, but now we have to keep track of the direction
-;; so we know how to "flip" the dial and work from the other side.
+;; This time we partition the dial positions and
+;; run [`count-crossings`](#count-crossings) on them to get the count
+;; of multiples and sum them up.
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 (defn part-2
   [input]
-  (:zeroes
-   (reduce (fn [{:keys [dial dir zeroes]} rot]
-             (let [[new-dir amount] (parse-rotation rot)
-                   dial (if (not= dir new-dir) (- 100 dial) dial)
-                   dial (+ (mod dial 100) amount)]
-               {:dial dial :dir new-dir :zeroes (+ zeroes (quot dial 100))}))
-           {:dial 50 :dir \R :zeroes 0}
-           input)))
+  (->> input
+       (partition 2 1)
+       (map #(apply count-crossings %))
+       (apply +)))
 
 ;; Which gives our answer
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
